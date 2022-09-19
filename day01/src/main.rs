de8e87a1cfeb6cce5b88b523906333b6428f1702
Cristobal <correodecristobal@gmail.com>
2022-09-18 23:19:41 -0600

    day02 - dive part 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -24,6 +24,18 @@ fn get_amount_of_increased_sweeps(sonar_sweeps: Vec<i32>) -> usize {
     filtered.count()
 }
 
+fn get_sum_of_window(sonar_sweeps: Vec<i32>) -> Vec<i32> {
+    let measurement_size = 3;
+    let sum_of_measurements = sonar_sweeps.windows(measurement_size).map(|x| x[0] + x[1] + x[2]).collect();
+    sum_of_measurements
+}
+
+fn get_amount_of_increased_measurement_windows(sonar_sweeps_sum: Vec<i32>) -> usize {
+    let measurement_size = 2;
+    let filtered = sonar_sweeps_sum.windows(measurement_size).filter(|x| x[0] < x[1]);
+    filtered.count()
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -60,5 +72,41 @@ mod tests {
         let amount_of_increase = get_amount_of_increased_sweeps(lines_to_int);
         assert_eq!(amount_of_increase, 1226);
     }
+    //part 2
+
+    #[test]
+    fn test_get_sum_of_window() {
+        let lines_file = get_lines_from_file("input2.txt");
+        let lines = lines_file.unwrap(); 
+        let lines_to_int = map_lines_to_int(lines);
+        let sum_vec = get_sum_of_window(lines_to_int);
+        let first_window_sum = sum_vec[0 as usize];
+        let second_window_sum = sum_vec[1 as usize];
+        assert_eq!(first_window_sum, 607);
+        assert_eq!(second_window_sum, 618);
+    }
+
+    #[test]
+    fn test_get_amount_of_increased_measurement_windows_mock_input() {
+        let lines_file = get_lines_from_file("input2.txt");
+        let lines = lines_file.unwrap(); 
+        let lines_to_int = map_lines_to_int(lines);
+        let sum_vec = get_sum_of_window(lines_to_int);
+        let amount_of_increase = get_amount_of_increased_sweeps(sum_vec);
+        assert_eq!(amount_of_increase, 5);
+    }
+
+    #[test]
+    fn test_get_amount_of_increased_measurement_windows() {
+        let lines_file = get_lines_from_file("input.txt");
+        let lines = lines_file.unwrap(); 
+        let lines_to_int = map_lines_to_int(lines);
+        let sum_vec = get_sum_of_window(lines_to_int);
+        let amount_of_increase = get_amount_of_increased_sweeps(sum_vec);
+        assert_eq!(amount_of_increase, 5);
+    }
+
+
+
 
 }
---
+++ day02/src/main.rs
@@ -0,0 +1,106 @@
+// --- Day 2: Dive! ---
+// https://adventofcode.com/2021/day/2
+
+// example
+// forward 5
+// down 5
+// forward 8
+// up 3
+// down 8
+// forward 2
+
+// after following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
+
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+use std::io::Result;
+
+
+fn main() {
+    println!("Dive!");
+}
+
+fn get_lines_from_file(filename: impl AsRef<Path>) -> Result<Vec<String>> {
+    BufReader::new(File::open(filename)?).lines().collect()
+}
+
+fn convert_line_to_movement(line: &str) -> (&str, i32)  {
+    let mut splitted = line.split_whitespace();
+    let direction = splitted.next().unwrap_or("");
+    let value = splitted.next().unwrap_or("0").parse::<i32>().unwrap(); 
+    (direction, value)
+}
+
+fn process_movement_instructions(lines: Vec<String>) -> (i32, i32) {
+    let mut horizontal_position = 0;
+    let mut depth_position = 0;
+
+    for line in lines {
+        let movement: (&str, i32) = convert_line_to_movement(&line);
+        match movement {
+            ("forward", _) => horizontal_position = horizontal_position + movement.1,
+            ("up", _) => depth_position = depth_position - movement.1,
+            ("down", _) => depth_position = depth_position + movement.1,
+            (&_, _) => horizontal_position = horizontal_position
+        }
+    }
+    (horizontal_position, depth_position)
+}
+
+fn get_final_position_multiplied(final_position: (i32, i32)) -> i32 {
+    final_position.0 * final_position.1
+}
+
+//struct SubmarineMovement
+// completed_movements<Vec<[string, i32]>>
+// impl move_to_position(string, i32)
+// current_position_horizontal
+// current_position_depth
+// get multiplied position
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_lines_from_file() {
+        let lines_file = get_lines_from_file("input.txt");
+        let lines = lines_file.unwrap(); 
+        let selected_line = &lines[1 as usize];
+        assert_eq!(selected_line, "forward 8");
+    }
+
+    #[test]
+    fn test_convert_line_into_movement() {
+        let lines_file = get_lines_from_file("input.txt");
+        let lines = lines_file.unwrap(); 
+        let selected_line = &lines[1 as usize];
+        let movement: (&str, i32) = convert_line_to_movement(selected_line);
+        assert_eq!(movement, ("forward", 8));
+    }
+
+    #[test]
+    fn test_process_movement_instructions() {
+        let lines_file = get_lines_from_file("input.txt");
+        let lines = lines_file.unwrap(); 
+        let processed_movements = process_movement_instructions(lines);
+        assert_eq!(processed_movements, (2162, 1051));
+    }
+
+    #[test]
+    fn test_get_final_position_multiplied() {
+        let lines_file = get_lines_from_file("input.txt");
+        let lines = lines_file.unwrap(); 
+        let processed_movements = process_movement_instructions(lines);
+        let final_position = get_final_position_multiplied(processed_movements);
+        assert_eq!(final_position, 2272262);
+    }
+}
+// TODO:
+// ✅ get inputs
+// ✅ parse inputs into direction, value
+// ✅ process parsed inputs: + - by direction
+// ✅ multiply processed horizontal and depth 

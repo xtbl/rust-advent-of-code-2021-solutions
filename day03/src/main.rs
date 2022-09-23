9c377a56962cf4fdcf80e8383f1c00b4e0514fbe
Cristobal <correodecristobal@gmail.com>
2022-09-22 22:07:14 -0600

    day03 - part 02

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,13 +1,17 @@
 // --- Day 3: Binary Diagnostic ---
 // https://adventofcode.com/2021/day/3
 
-
+// part 02 - life support rating
+// multiplying the oxygen generator rating by the CO2 scrubber rating
+// get full list input
+// consider just the first bit
 
 
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::io::Result;
+use std::str::FromStr;
 
 
 fn main() {
@@ -49,9 +53,7 @@ fn calculate_power_comsumption(gamma: i32, epsilon: i32) -> i32 {
 fn get_gamma(bits_at_position: &Vec<i32>) -> i32 {
     let filter_0: Vec<&i32> = bits_at_position.iter().filter(|x| *x == &0 as &i32).collect();
     let filter_1: Vec<&i32> = bits_at_position.iter().filter(|x| *x == &1 as &i32).collect();
-    println!("filter_0: {:?}", filter_0);
-    println!("filter_1: {:?}", filter_1);
-    if filter_0.iter().count() >  filter_1.iter().count(){
+    if filter_0.iter().count() > filter_1.iter().count(){
        0
     } else {
        1
@@ -68,6 +70,39 @@ fn get_epsilon(bits_at_position: &Vec<i32>) -> i32 {
     }
 }
 
+fn get_most_common_value_at_position(lines: &Vec<String>, pos: usize) -> i32 {
+    let bits_at_position = get_all_bits_at_position(&lines, pos);
+    get_gamma(&bits_at_position)
+}
+
+fn get_least_common_value_at_position(lines: &Vec<String>, pos: usize) -> i32 {
+    let bits_at_position = get_all_bits_at_position(&lines, pos);
+    get_epsilon(&bits_at_position)
+}
+
+fn get_oxygen_generator_rating(lines: &Vec<String>, is_oxygen: bool) -> i32 {
+    let mut filtered_lines: Vec<String> = lines.clone();    
+    let mut current_position: usize = 0;
+
+    while filtered_lines.len() > 1  {
+        let bit_criteria_integer = if is_oxygen {get_most_common_value_at_position(&filtered_lines, current_position)} else {get_least_common_value_at_position(&filtered_lines, current_position)};
+        let bit_criteria_integer = char::from_digit(bit_criteria_integer as u32, 10).unwrap();
+        filtered_lines = filtered_lines.iter().cloned().filter(|x| {
+            x.chars().nth(current_position).unwrap() == bit_criteria_integer
+        }).collect::<Vec<_>>();
+
+        current_position = current_position + 1;
+    }
+    let vec_to_binary: Vec<String> = filtered_lines.iter().map(|x| x.to_string()).collect();
+    let join_binary_string = vec_to_binary.join("");
+    let into_decimal = i32::from_str_radix(&join_binary_string, 2).unwrap_or(0);
+    into_decimal
+}
+
+fn life_support_rating(a: i32, b: i32) -> i32 {
+   a * b 
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -76,7 +111,7 @@ mod tests {
     #[test]
     fn test_get_lines_from_file() {
         let lines_file = get_lines_from_file("mock_input.txt");
-        let lines = lines_file.unwrap(); 
+        let lines = lines_file.unwrap();
         let selected_line = &lines[1 as usize];
         assert_eq!(selected_line, "11110");
     }
@@ -84,15 +119,15 @@ mod tests {
     #[test]
     fn test_get_all_bits_at_position() {
         let lines_file = get_lines_from_file("mock_input.txt");
-        let lines = lines_file.unwrap(); 
+        let lines = lines_file.unwrap();
         let bits_at_position = get_all_bits_at_position(&lines, 0);
-        assert_eq!(bits_at_position,[0,1,1,1,1,0,0,1,1,1,0,0])        
+        assert_eq!(bits_at_position,[0,1,1,1,1,0,0,1,1,1,0,0])       
     }
 
     #[test]
     fn test_get_gamma() {
         let lines_file = get_lines_from_file("mock_input.txt");
-        let lines = lines_file.unwrap(); 
+        let lines = lines_file.unwrap();
         let bits_at_position = get_all_bits_at_position(&lines, 0);
         assert_eq!(get_gamma(&bits_at_position), 1);
     }
@@ -100,7 +135,7 @@ mod tests {
     #[test]
     fn test_get_epsilon() {
         let lines_file = get_lines_from_file("mock_input.txt");
-        let lines = lines_file.unwrap(); 
+        let lines = lines_file.unwrap();
         let bits_at_position = get_all_bits_at_position(&lines, 0);
         assert_eq!(get_epsilon(&bits_at_position), 0);
     }
@@ -108,11 +143,39 @@ mod tests {
     #[test]
     fn test_get_rate_for_all_positions() {
         let lines_file = get_lines_from_file("input.txt");
-        let lines = lines_file.as_ref().unwrap(); 
+        let lines = lines_file.as_ref().unwrap();
         let gamma_rate = get_rate_for_all_positions(&lines, true);
         let epsilon_rate = get_rate_for_all_positions(&lines, false);
         let power_comsumption = calculate_power_comsumption(gamma_rate, epsilon_rate);
         assert_eq!(power_comsumption, 845186);
+    }
+
+    #[test]
+    fn test_get_oxygen_generator_rating() {
+        let lines_file = get_lines_from_file("mock_input.txt");
+        let lines = lines_file.unwrap(); 
+        assert_eq!(get_oxygen_generator_rating(&lines, true), 23);
+    }
+
+    #[test]
+    fn test_get_co2_scrubber_rating() {
+        let lines_file = get_lines_from_file("mock_input.txt");
+        let lines = lines_file.unwrap(); 
+        assert_eq!(get_oxygen_generator_rating(&lines, false), 10);
+    }
+
+    #[test]
+    fn test_life_support_rating_mock() {
+        let lines_file = get_lines_from_file("mock_input.txt");
+        let lines = lines_file.unwrap(); 
+        assert_eq!(get_oxygen_generator_rating(&lines, true) * get_oxygen_generator_rating(&lines, false), 230);
+    }
+
+    #[test]
+    fn test_life_support_rating() {
+        let lines_file = get_lines_from_file("input.txt");
+        let lines = lines_file.unwrap(); 
+        assert_eq!(get_oxygen_generator_rating(&lines, true) * get_oxygen_generator_rating(&lines, false), 4636702);
     }
 
 }
